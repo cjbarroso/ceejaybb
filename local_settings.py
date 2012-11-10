82067518424b34742bc179f249a20ed1a380cf40
Carlos Barroso <cbarroso@machinalis.com>
2012-11-10 01:50:56 -0300

    Deleted local_settings from repo

--- local_settings.py
+++
@@ -1,8 +0,0 @@
-import os
-TKEY="e6802882008b3b57f64629336e9c075b"
-TOKEN="f7408c2baa96d2d174d2a1c8d09018d16c6a64ca91df67ef8d88309f4272816b"
-LOG_DEST=os.path.expanduser("~/irclogs/machinalis-cpi.log")
-CONFIGFILE=os.path.expanduser("~/.ceejaybbrc")
-
-BANNER_STANDUP_BEGIN=">>============== Standup begin ============<<"
-BANNER_STANDUP_END=">>============== Standup end ============<<"
